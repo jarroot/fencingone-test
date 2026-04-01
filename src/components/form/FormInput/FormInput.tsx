e8f9c927e35ee0b18ff1c1654bff9cff609e48bc
Vadim Ponamarenko <jarroot1@gmail.com>
2026-04-01 15:36:07 +0300

    FormInput improved for better performace

--- src/components/form/FormInput/FormInput.tsx
+++ src/components/form/FormInput/FormInput.tsx
@@ -1,8 +1,25 @@
-import { memo } from "react";
+import { memo, useState, useMemo, useEffect } from "react";
 import { FormInputProps } from "./FormInput.model";
+import { getDebounceFn } from "utils/functions";
 import "./FormInput.css";
 
 export default memo( function FormInput({ id, name, type, label, value, style, placeholder, EndIcon, onChange }: FormInputProps ) {
+
+   const [ local, setLocal ] = useState( value );
+
+   const debouncedOnChange = useMemo(() => {
+      return onChange ? getDebounceFn( onChange ) : null
+   }, [])
+
+   const handleChange = ( e: any ) => {
+      const value = e.target.value as string;
+      setLocal( value );
+      debouncedOnChange && debouncedOnChange( name, value );
+   }
+
+   useEffect(() => {
+      setLocal( value );
+   }, [ value ])
 
    return (
       <div className="form-input" style={ style }>
@@ -16,11 +33,8 @@ export default memo( function FormInput({ id, name, type, label, value, style, p
             name={ name }
             type={ type }
             placeholder={ placeholder }
-            value={ value }
-            onChange={ e => {
-               const value = e.target.value as string;
-               onChange && onChange( name, value );
-            }}
+            value={ local }
+            onChange={ handleChange }
          />
 
          { EndIcon }
